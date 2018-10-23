929b347300ee2c4ce3a5939367decf8494a6f96f
f0rtung <zadov.vasya@gmail.com>
2018-10-24 00:17:57 +0300

    Default\Delete ctors

--- src/file-hash-consumer/hash.h
+++ src/file-hash-consumer/hash.h
@@ -8,6 +8,10 @@ namespace file_signature {
     {
     public:
         hash_t( const std::string &hash, std::size_t idx );
+        hash_t( hash_t & ) = delete;
+        hash_t &operator=( hash_t & ) = delete;
+        hash_t( hash_t && ) = default;
+        hash_t &operator=( hash_t && ) = default;
         const std::string &hash( ) const noexcept;
         std::size_t index( ) const noexcept;
 
